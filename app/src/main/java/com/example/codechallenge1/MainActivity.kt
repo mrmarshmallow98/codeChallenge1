5185cf4025f9b2a63c0c73c1186cdb17725c4c23
mrmarshmallow98 <ahjie981127@gmail.com>
2019-10-23 09:06:49 +0800

    Code challenge

--- app/src/main/java/com/example/codechallenge1/MainActivity.kt
+++ app/src/main/java/com/example/codechallenge1/MainActivity.kt
@@ -20,25 +20,48 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val randomInt = Random().nextInt(6) + 1
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+
+        val resultText1: TextView = findViewById(R.id.result_text1)
+        val resultText2: TextView = findViewById(R.id.result_text2)
+        val resultText3: TextView = findViewById(R.id.result_text3)
+
+        val randomInt1 = Random().nextInt(6) + 1
+        resultText1.text = randomInt1.toString()
+
+        val randomInt2 = Random().nextInt(6) + 1
+        resultText2.text = randomInt2.toString()
+
+        val randomInt3 = Random().nextInt(6) + 1
+        resultText3.text = randomInt3.toString()
     }
 
 
     private fun countUp() {
 
-        val resultText: TextView = findViewById(R.id.result_text)
+        val resultText1: TextView = findViewById(R.id.result_text1)
+        val resultText2: TextView = findViewById(R.id.result_text2)
+        val resultText3: TextView = findViewById(R.id.result_text3)
 
-        if (resultText.text == "Hello World!") {
-            resultText.text = "1"
+        if ((resultText1.text == "Hello World!")&&(resultText2.text == "Hello World!")&&(resultText3.text == "Hello World!")) {
+            resultText1.text = "1"
+            resultText2.text = "1"
+            resultText3.text = "1"
         } else {
-            var resultInt = resultText.text.toString().toInt()
+            var resultInt1 = resultText1.text.toString().toInt()
+            var resultInt2 = resultText2.text.toString().toInt()
+            var resultInt3 = resultText3.text.toString().toInt()
 
-            if (resultInt < 6) {
-                resultInt++
-                resultText.text = resultInt.toString()
+
+            if ((resultInt1 < 6)&&(resultInt2 < 6)&&(resultInt3 < 6)) {
+                resultInt1++
+                resultText1.text = resultInt1.toString()
+
+                resultInt2++
+                resultText2.text = resultInt2.toString()
+
+                resultInt3++
+                resultText3.text = resultInt3.toString()
             }
         }
     }
